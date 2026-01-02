9ac1aa29b7f0ba68f707f7229a5b13fe880dd708
SASCHADDD <saschadanu23@gmail.com>
2026-01-02 10:19:44 +0700

    menambahkan dependecies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,20 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.analytics)
+    implementation(libs.coroutines.play.services)
+
+// navigasi
+    implementation(libs.navigation.compose)
+
+// viewmodel
+    implementation(libs.lifecycle.viewmodel.compose)
+    implementation(libs.lifecycle.runtime.compose)
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
