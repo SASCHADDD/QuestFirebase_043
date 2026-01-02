41ba051949d6dd307e519a72226796857f48dcd8
SASCHADDD <saschadanu23@gmail.com>
2026-01-02 19:22:37 +0700

    mengakses tabel siswa dari firesstore

--- app/src/main/java/com/example/pertemuan14/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/pertemuan14/repositori/RepositorySiswa.kt
@@ -1,8 +1,13 @@
 package com.example.pertemuan14.repositori
 
 import com.example.pertemuan14.modeldata.Siswa
+import com.google.firebase.firestore.FirebaseFirestore
 
 interface RepositorySiswa{
     suspend fun getDataSiswa(): List<Siswa>
     suspend fun postDataSiswa(siswa : Siswa)
 }
+class FirebaseRepositorySiswa : RepositorySiswa{
+    private val db = FirebaseFirestore.getInstance()
+    private val collection = db.collection("siswa")
+
